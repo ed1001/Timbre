ead8ad98d30a188126706e28da6b657ec53e5c4f
Edward Phillips <edphil@hotmail.co.uk>
2019-09-01 00:41:47 +0200

    started on clean up

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
+  validates :body, presence: true, allow_blank: false
+
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
