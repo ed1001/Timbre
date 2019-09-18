7df470f462f7b05cc256d99610408fe79a714c42
Edward Phillips <edphil@hotmail.co.uk>
2019-09-18 12:08:39 +0200

    added validation for position attribute on photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   validates :photo, presence: true
+  validates :position, presence: true, inclusion: 0..5, uniqueness: { scope: :user_id }
 
   mount_uploader :photo, PhotoUploader
 end
